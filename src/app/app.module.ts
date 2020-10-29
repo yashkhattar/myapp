fa81f45aabf7e911bfe87ec4bbca4198300f17d1
yashkhattar <khattaryash1997@gmail.com>
2020-10-29 18:00:14 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsonComponent } from './json/json.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChildComponent } from './child/child.component';
+import { NewComponent } from './new/new.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonComponent,
+    ChildComponent,
+    NewComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
