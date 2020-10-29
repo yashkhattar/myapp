df6c4f4e41143b2c7beb34a2ca93625baf5ab85a
yashkhattar <khattaryash1997@gmail.com>
2020-10-29 17:59:56 +0530

    Create api.service.ts

---
+++ src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) {}
+   getData() : Observable<any> {
+     const url="https://jsonplaceholder.typicode.com/posts"
+     return this.http.get<any>(url)
+   }
+
+  
+  }
+
