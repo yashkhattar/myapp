8548f4a52f679c01b15a4fd79c493024bb7706ab
yashkhattar <khattaryash1997@gmail.com>
2020-10-29 18:00:09 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import {JsonComponent} from './json/json.component';
+import {NewComponent} from './new/new.component';
+import {TileComponent} from './tile/tile.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'json' , component: JsonComponent},
+  {path: 'child' , component: ChildComponent},
+  {path: 'new' , component: NewComponent},
+  {path: 'tile' , component: TileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
